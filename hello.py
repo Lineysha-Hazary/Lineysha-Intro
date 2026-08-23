f5cca0c1e1ee11a3dbedc08fd4c1ca0272eeb630
Lineysha-Hazary <lineysha.hazary.08@gmail.com>
2026-08-23 11:26:33 +0530

    Intro.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+print("Hello!"," ",name)
+hru = input("Hey, Is your day going well? (y/n):")
+if hru =="y":
+  print("Thats great!)
+else:
+  print("I hope you feel better now")
+  
